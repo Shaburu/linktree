f61acc858eaa12a0652073aafd576a694add4c5a
shaburu <xshaburu@gmail.com>
2022-12-15 00:35:32 +0530

    Update Newsletter.js

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -4,31 +4,20 @@ import './foot.css'
 const Newsletter = () => {
   return (
     <div class="newsletter">
-        <form class="form">
-            <div class="input-group">
-                <input type="email" class="input" placeholder="Drop Your Email!"></input>
+//      
+//        <form class="form">
+//            <div class="input-group">
+//                <input type="email" class="input" placeholder="Drop Your Email!"></input>
                 
                 <label class="placeholder">
-                <lord-icon
-    src="https://cdn.lordicon.com/lupuorrc.json"
-    trigger="loop"
-    class="lord-icon"
-    >
-</lord-icon>
-                  Sign Up For Everything Shaburu!        
-                  <lord-icon
-    src="https://cdn.lordicon.com/lupuorrc.json"
-    trigger="loop"
-    class="lord-icon"
-    >
-</lord-icon>
+               
                 </label>
                 
             </div>
             
         </form>
         
-    
+  
     </div>
   )
 }
